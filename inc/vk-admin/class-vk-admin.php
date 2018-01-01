b40214f78b4aa41f01a4f12f02dbf68bce6bc9a0
kurudrive <kurudrive@gmail.com>
2018-01-01 14:19:08 +0900

    不要ファイル削除など

--- inc/vk-admin/class-vk-admin.php
+++ inc/vk-admin/class-vk-admin.php
@@ -11,7 +11,11 @@ https://github.com/vektor-inc/vektor-wp-libraries
 
 if ( ! class_exists( 'Vk_Admin' ) )
 {
-
+/*
+congif.phpの方で既に ! class_exists( 'Vk_Admin' ) しているが、
+今後読み込みファイルが増えた時にVk-Adminの中で別のファイルを読み込むために
+このファイルにも更にclass_exists( 'Vk_Admin' ) がある。
+ */
 class Vk_Admin {
 
 	public static $version = '2.0.0';
