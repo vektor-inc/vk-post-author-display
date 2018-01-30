f898782a5413051527301849a7178c0e2e53370c
kurudrive <kurudrive@gmail.com>
2018-01-30 12:19:08 +0900

    [ term color ] update

--- inc/term-color/class.term-color.php
+++ inc/term-color/class.term-color.php
@@ -8,7 +8,7 @@ https://github.com/vektor-inc/vektor-wp-libraries
 
 if ( ! class_exists( 'Vk_term_color' ) ) {
 
-    class Vk_term_color {
+	class Vk_term_color {
 
 		/*-------------------------------------------*/
 		/*  REGISTER TERM META
@@ -16,17 +16,17 @@ if ( ! class_exists( 'Vk_term_color' ) ) {
 
 		function term_meta_color() {
 
-		    register_meta( 'term', 'term_color', array( $this, 'sanitize_hex') );
+			register_meta( 'term', 'term_color', array( $this, 'sanitize_hex' ) );
 		}
 
 		/*-------------------------------------------*/
 		/*  SANITIZE DATA
 		/*-------------------------------------------*/
 
-		public static function sanitize_hex ( $color ) {
+		public static function sanitize_hex( $color ) {
 			// sanitize_hex_color() は undefined function くらう
 			$color = ltrim( $color, '#' );
-		    return preg_match( '/([A-Fa-f0-9]{3}){1,2}$/', $color ) ? $color : '';
+			return preg_match( '/([A-Fa-f0-9]{3}){1,2}$/', $color ) ? $color : '';
 		}
 
 		/*-------------------------------------------*/
@@ -34,7 +34,7 @@ if ( ! class_exists( 'Vk_term_color' ) ) {
 		/*-------------------------------------------*/
 		function taxonomy_add_new_meta_field_color() {
 			global $vk_term_color_textdomain;
-			
+
 			// this will add the custom meta field to the add new term page
 			?>
 			<div class="form-field">
@@ -48,7 +48,7 @@ if ( ! class_exists( 'Vk_term_color' ) ) {
 		/*-------------------------------------------*/
 		/*	タクソノミー編集ページでのフォーム
 		/*-------------------------------------------*/
-		function taxonomy_add_edit_meta_field_color($term) {
+		function taxonomy_add_edit_meta_field_color( $term ) {
 			global $vk_term_color_textdomain;
 			// put the term ID into a variable
 			$term_color = Vk_term_color::get_term_color( $term->term_id );
@@ -69,9 +69,10 @@ if ( ! class_exists( 'Vk_term_color' ) ) {
 		// Save extra taxonomy fields callback function.
 		function save_term_meta_color( $term_id ) {
 
-		    // verify the nonce --- remove if you don't care
-		    if ( ! isset( $_POST['term_color_nonce'] ) || ! wp_verify_nonce( $_POST['term_color_nonce'], basename( __FILE__ ) ) )
-		        return;
+			// verify the nonce --- remove if you don't care
+			if ( ! isset( $_POST['term_color_nonce'] ) || ! wp_verify_nonce( $_POST['term_color_nonce'], basename( __FILE__ ) ) ) {
+				return;
+			}
 
 			if ( isset( $_POST['term_color'] ) ) {
 				$now_value = get_term_meta( $term_id, 'term_color', true );
@@ -82,7 +83,7 @@ if ( ! class_exists( 'Vk_term_color' ) ) {
 					add_term_meta( $term_id, 'term_color', $new_value );
 				}
 			}
-		} 
+		}
 
 		/*-------------------------------------------*/
 		/* 	管理画面 _ カラーピッカーのスクリプトの読み込み
@@ -90,32 +91,36 @@ if ( ! class_exists( 'Vk_term_color' ) ) {
 
 		function admin_enqueue_scripts( $hook_suffix ) {
 
-		    // if ( 'edit-tags.php' !== $hook_suffix || 'category' !== get_current_screen()->taxonomy )
-		    //     return;
+			// if ( 'edit-tags.php' !== $hook_suffix || 'category' !== get_current_screen()->taxonomy )
+			//     return;
 
-		    wp_enqueue_style( 'wp-color-picker' );
-		    wp_enqueue_script( 'wp-color-picker' );
+			wp_enqueue_style( 'wp-color-picker' );
+			wp_enqueue_script( 'wp-color-picker' );
 
-		    // add_action( 'admin_head',   'lmu_term_colors_print_styles' );
-		    add_action( 'admin_footer', array( $this, 'term_colors_print_scripts' ) );
+			// add_action( 'admin_head',   'lmu_term_colors_print_styles' );
+			add_action( 'admin_footer', array( $this, 'term_colors_print_scripts' ) );
 		}
 
-		function term_colors_print_styles() { ?>
+		function term_colors_print_styles() {
+		?>
 
-		    <style type="text/css">
-		        .column-color { width: 50px; }
-		        .column-color .color-block { display: inline-block; width: 28px; height: 28px; border: 1px solid #ddd; }
-		    </style>
-		<?php }
+			<style type="text/css">
+				.column-color { width: 50px; }
+				.column-color .color-block { display: inline-block; width: 28px; height: 28px; border: 1px solid #ddd; }
+			</style>
+		<?php
+		}
 
-		function term_colors_print_scripts() { ?>
+		function term_colors_print_scripts() {
+		?>
 
-		    <script type="text/javascript">
-		        jQuery( document ).ready( function( $ ) {
-		            $( '.term_color' ).wpColorPicker();
-		        } );
-		    </script>
-		<?php }
+			<script type="text/javascript">
+				jQuery( document ).ready( function( $ ) {
+					$( '.term_color' ).wpColorPicker();
+				} );
+			</script>
+		<?php
+		}
 
 		/*-------------------------------------------*/
 		/* 	管理画面 _ カテゴリー一覧でカラムの追加
@@ -124,68 +129,91 @@ if ( ! class_exists( 'Vk_term_color' ) ) {
 		function edit_term_columns( $columns ) {
 			global $vk_term_color_textdomain;
 
-		    $columns['color'] = __( 'Color', $vk_term_color_textdomain );
+			$columns['color'] = __( 'Color', $vk_term_color_textdomain );
 
-		    return $columns;
+			return $columns;
 		}
 
 
 		function manage_term_custom_column( $out, $column, $term_id ) {
 
-		    if ( 'color' === $column ) {
+			if ( 'color' === $column ) {
 
-		        $color = Vk_term_color::get_term_color( $term_id );
+				$color = Vk_term_color::get_term_color( $term_id );
 
-		        if ( ! $color )
-		            $color = '#ffffff';
+				if ( ! $color ) {
+					$color = '#ffffff';
+				}
 
-		        $out = sprintf( '<span class="color-block" style="background:%s;">&nbsp;</span>', esc_attr( $color ) );
-		    }
+				$out = sprintf( '<span class="color-block" style="background:%s;">&nbsp;</span>', esc_attr( $color ) );
+			}
 
-		    return $out;
+			return $out;
 		}
 
 		/*-------------------------------------------*/
 		/* 	termのカラーを取得
 		/*-------------------------------------------*/
-		public static function get_term_color( $term_id ){
+		public static function get_term_color( $term_id ) {
 			$term_color_default = '#999999';
 			$term_color_default = apply_filters( 'term_color_default_custom', $term_color_default );
 			if ( isset( $term_id ) ) {
-				$term_color = Vk_term_color::sanitize_hex ( get_term_meta( $term_id, 'term_color', true ) );
-				$term_color = ( $term_color ) ? '#'.$term_color : $term_color_default;
+				$term_color = Vk_term_color::sanitize_hex( get_term_meta( $term_id, 'term_color', true ) );
+				$term_color = ( $term_color ) ? '#' . $term_color : $term_color_default;
 			} else {
 				$term_color = $term_color_default;
 			}
 			return $term_color;
 		}
 
-        /*-------------------------------------------*/
-        /*  実行
-        /*-------------------------------------------*/
-        public function __construct(){
-            add_action( 'init', array( $this, 'term_meta_color' ) );
-            add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
+		/*-------------------------------------------*/
+		/* 	term color を有効化する taxonomy
+		/*-------------------------------------------*/
+		public function get_term_color_taxonomies() {
+			/*
+			最初Global変数指定をしていたが、 Global変数では
+			複数の term color が存在した場合に実行タイミングの都合上任意に指定が効かないため、
+			フックでの指定を行う
+			 */
+			global $vk_term_color_taxonomies;
+			if ( $vk_term_color_taxonomies ) {
+				$taxonomies = $vk_term_color_taxonomies;
+			} else {
+				$taxonomies = array( 'category', 'post_tag' );
+			}
+			$taxonomies = apply_filters( 'term_color_taxonomies_custom', $taxonomies );
+			// 重複の値を削除
+			$taxonomies = array_unique( $taxonomies );
+			// 特に影響はないがキーを振り直す
+			$taxonomies = array_values( $taxonomies );
+			return $taxonomies;
+		}
+		/*-------------------------------------------*/
+		/*  実行
+		/*-------------------------------------------*/
+		public function __construct() {
+			add_action( 'init', array( $this, 'term_meta_color' ) );
+			add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
 
 			/*-------------------------------------------*/
 			/* 	管理画面 _ 各種処理発火
 			/*-------------------------------------------*/
 			// カラーピッカーを追加するタクソノミー
-			$taxonomies = array('category','post_tag');
+
+			$taxonomies = Vk_term_color::get_term_color_taxonomies();
 
 			// 該当のタクソノミー分ループ処理する
-			foreach ($taxonomies as $key => $value) {
-				add_action( $value.'_add_form_fields', array( $this, 'taxonomy_add_new_meta_field_color' ), 10, 2 );
-				add_action( $value.'_edit_form_fields', array( $this, 'taxonomy_add_edit_meta_field_color' ), 10, 2 );
-				add_action( 'edited_'.$value, array( $this, 'save_term_meta_color' ), 10, 2 );  
-				add_action( 'create_'.$value, array( $this, 'save_term_meta_color' ), 10, 2 );
-				add_filter( 'manage_edit-'.$value.'_columns', array( $this, 'edit_term_columns' ) );
-				add_filter( 'manage_'.$value.'_custom_column', array( $this, 'manage_term_custom_column' ), 10, 3 );
+			foreach ( $taxonomies as $key => $value ) {
+				add_action( $value . '_add_form_fields', array( $this, 'taxonomy_add_new_meta_field_color' ), 10, 2 );
+				add_action( $value . '_edit_form_fields', array( $this, 'taxonomy_add_edit_meta_field_color' ), 10, 2 );
+				add_action( 'edited_' . $value, array( $this, 'save_term_meta_color' ), 10, 2 );
+				add_action( 'create_' . $value, array( $this, 'save_term_meta_color' ), 10, 2 );
+				add_filter( 'manage_edit-' . $value . '_columns', array( $this, 'edit_term_columns' ) );
+				add_filter( 'manage_' . $value . '_custom_column', array( $this, 'manage_term_custom_column' ), 10, 3 );
 			}
-        }
-    }
+		}
+	}
 
-    $Vk_term_color = new Vk_term_color();
-    
+	$Vk_term_color = new Vk_term_color();
+
 }
-
