6815d3087f97e6d7512f4be040522c9b6767c31c
kurudrive <kurudrive@gmail.com>
2018-09-30 01:27:43 +0900

    [ font awesome ]

--- inc/font-awesome-config.php
+++ inc/font-awesome-config.php
@@ -4,15 +4,36 @@
 /*  Load modules
 /*-------------------------------------------*/
 if ( ! class_exists( 'Vk_Font_Awesome_Versions' ) ) {
-	require_once( 'font-awesome/class-vk-font-awesome-versions.php' );
 
-	global $font_awesome_directory_uri;
-	$font_awesome_directory_uri = plugins_url( '', __FILE__ ) . '/font-awesome/';
+	// テーマがLightningかどうか
+	function pad_is_theme_lightning() {
+		$template = get_option( 'template' );
+		if ( $template == 'lightning' || $template == 'Lightning' || $template == 'lightning-pro' ) {
+			return true;
+		}
+	}
 
-	global $vk_font_awesome_version_textdomain;
-	$vk_font_awesome_version_textdomain = 'vkExUnit';
+	// ExUnitでFontAwesomeがアクティブになってるかどうか？
+	function pad_is_veu_fa_active() {
+		 include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+		 $plugin = 'vk-all-in-one-expansion-unit/vkExUnit.php';
+		if ( is_plugin_active( $plugin ) ) {
+			$options = vkExUnit_get_common_options();
+			if ( empty( $options['active_fontawesome'] ) ) {
+				return true;
+			}
+		}
+	}
 
-	global $set_enqueue_handle_style;
-	$set_enqueue_handle_style = 'vkExUnit_common_style';
+	if ( ! pad_is_theme_lightning() && ! pad_is_veu_fa_active() ) {
 
+		require_once( 'font-awesome/class-vk-font-awesome-versions.php' );
+
+		global $font_awesome_directory_uri;
+		$font_awesome_directory_uri = plugins_url( '', __FILE__ ) . '/font-awesome/';
+
+		global $set_enqueue_handle_style;
+		$set_enqueue_handle_style = 'vkExUnit_common_style';
+
+	}
 }
