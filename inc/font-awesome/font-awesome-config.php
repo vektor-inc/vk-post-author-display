b00cf25ab94794b4ce5d137c450aa827e3f85a09
drill-lancer <rickaddison7634@gmail.com>
2022-03-04 15:33:59 +0900

    fix: da url

--- inc/font-awesome/font-awesome-config.php
+++ inc/font-awesome/font-awesome-config.php
@@ -14,5 +14,5 @@ if ( ! class_exists( 'Vk_Font_Awesome_Versions' ) ) {
 	new VkFontAwesomeVersions();
 	global $font_awesome_directory_uri;
 	// phpcs:ignore
-	$font_awesome_directory_uri = VK_PAD_DIR . 'vendor/vektor-inc/font-awesome-versions/src/';
+	$font_awesome_directory_uri = VK_PAD_URL . 'vendor/vektor-inc/font-awesome-versions/src/';
 }
